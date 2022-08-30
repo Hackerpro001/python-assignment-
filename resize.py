ff0c4a41a743365c2ed0eab936241ac1fbea0dba
HANJAD <111428996+HANJAD@users.noreply.github.com>
2022-08-30 22:17:24 +0100

    Update resize.py

--- resize.py
+++ resize.py
@@ -3,5 +3,5 @@ import cv2
 def resize_small():
   scale_percent=45
   while True:
-    os.path(input("""
-          Enter the image directory    """))
+    pic=input("""
+          Enter the image directory    """)
