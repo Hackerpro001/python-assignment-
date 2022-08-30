8b03f40f18a210e646f49ccdf5149e1191bad15d
HANJAD <111428996+HANJAD@users.noreply.github.com>
2022-08-30 17:24:44 +0100

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,11 @@
+# python-assignment-
+import cv2
+import os.path
+print("welcome to the COMMANDLINE photoeditor")
+action=input("""what do you want to do
+      resize
+      blur
+      black and white""")
+if 
+      
+      
