60431fbb36a4cb3de2556ec93dc37e37256c40a9
hrhfreedom <112029969+hrhfreedom@users.noreply.github.com>
2022-09-06 01:10:43 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,22 @@
 # python-assignment-
 
+
+#cropping an image in the photoeditor
+import cv2
+#read input image
+img = cv2.imread(input('enter your picture file name: '))
+#check the type of read image
+print(type(img))
+#shape of the image
+print('Shape of the image',img.shape)
+#[rows,columns]
+crop = img[50:180 , 100:300]
+#Display the image
+cv2.imshow('original',img)
+cv2.imshow('cropped',crop)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
