3f5ba9b344150f33ac3222b7afa6ed412e86dbc9
1f3anyi <112389415+1f3anyi@users.noreply.github.com>
2022-09-07 16:26:58 +0200

    Update README.py

--- README.py
+++ README.py
@@ -231,5 +231,12 @@ print(img1)
 img2 = ImageColor.getcolor("red",'L')
 print(img2)
 
+#code that turns it to black and white
+img = cv2.imread(pic)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+status = cv2.imwrite(pic, gray_img)
 
-      
+cv2.imshow("Original", img)
+cv2.imshow("Gray Scaled", gray_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
